1e5128034c81be4bfd925b407c670bd29b145dab
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-14 18:43:03 +0530

    update random method in script

--- dice challenge/script.js
+++ dice challenge/script.js
@@ -1,23 +1,15 @@
-
-
-function draw(){
-    image1=document.getElementsByClassName("image")[0];
-    image2=document.getElementsByClassName("image")[1];
-    num1="images/dice"+Math.floor(Math.random()*6)+".png";
-    num2="images/dice"+Math.floor(Math.random()*6)+".png";
-    image1.src=num1;
-    image2.src=num2;
-    if(num1>num2)
-    {
-        document.getElementById("heading").innerHTML="Player1 wins🚩";
-    }
-    else if(num1==num2){
-        document.getElementById("heading").innerHTML=" Draw ";
-    }
-    else
-    {
-        document.getElementById("heading").innerHTML="Player2 wins🚩";
-    }
+function draw() {
+  image1 = document.getElementsByClassName("image")[0];
+  image2 = document.getElementsByClassName("image")[1];
+  num1 = "images/dice" + Math.floor(Math.random() * 5 + 1) + ".png";
+  num2 = "images/dice" + Math.floor(Math.random() * 5 + 1) + ".png";
+  image1.src = num1;
+  image2.src = num2;
+  if (num1 > num2) {
+    document.getElementById("heading").innerHTML = "Player1 wins🚩";
+  } else if (num1 == num2) {
+    document.getElementById("heading").innerHTML = " Draw ";
+  } else {
+    document.getElementById("heading").innerHTML = "Player2 wins🚩";
+  }
 }
-
-
